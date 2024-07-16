f912cc66f2f4328309a3a2bb6755211daa08b2c0
Akshat Jaiswal <akshatjaiswal.official@gmail.com>
2024-07-16 16:06:52 +0530

    feat: Added Exercises API.

---
+++ controllers/ExerciseController.js
@@ -0,0 +1,62 @@
+import exerciseData from "../data/exercise-data.json" assert { type: "json" };
+
+export const getExercises = async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit) || 10;
+    const page = parseInt(req.query.page) || 1;
+    const equipment = req.query.equipment;
+    const targetMuscle = req.query.target;
+    const bodyPart = req.query.bodyPart;
+    const search = req.query.search;
+
+    let filteredExercises = exerciseData;
+    if (search) {
+      const searchRegex = new RegExp(search, "i");
+      filteredExercises = filteredExercises.filter(
+        (exercise) =>
+          searchRegex.test(exercise.name) ||
+          searchRegex.test(exercise.title) ||
+          searchRegex.test(exercise.target) ||
+          searchRegex.test(exercise["muscle worked"]) ||
+          searchRegex.test(exercise.bodyPart) ||
+          searchRegex.test(exercise.equipment) ||
+          searchRegex.test(exercise.blog) ||
+          exercise.keywords.some((keyword) =>
+            searchRegex.test(keyword.toLowerCase()),
+          ),
+      );
+    }
+    if (equipment) {
+      const equipmentRegex = new RegExp(equipment, "i");
+      filteredExercises = filteredExercises.filter((exercise) =>
+        equipmentRegex.test(exercise.equipment),
+      );
+    }
+
+    if (targetMuscle) {
+      const targetRegex = new RegExp(targetMuscle, "i");
+      filteredExercises = filteredExercises.filter((exercise) =>
+        targetRegex.test(exercise.target),
+      );
+    }
+
+    if (bodyPart) {
+      const bodyPartRegex = new RegExp(bodyPart, "i");
+      filteredExercises = filteredExercises.filter((exercise) =>
+        bodyPartRegex.test(exercise.bodyPart),
+      );
+    }
+
+    const startIndex = page * limit;
+    const endIndex = startIndex + limit;
+    const paginatedData = filteredExercises.slice(startIndex, endIndex);
+    const totalPages = Math.ceil(filteredExercises.length / limit);
+    res.send({
+      totalExercises: filteredExercises.length,
+      totalPages,
+      data: paginatedData,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch exercises" });
+  }
+};
--- server.js
+++ server.js
@@ -2,11 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import exerciseRoutes from './routes/exerciseRoutes.js';
+
 dotenv.config()
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/v1/exercise", exerciseRoutes);
 
 app.use("/", (req, res) => {
     res.send({
