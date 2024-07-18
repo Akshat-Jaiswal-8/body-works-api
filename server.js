2ce6a915f948bf1db17e8a064b4f513a40f63951
Akshat Jaiswal <akshatjaiswal.official@gmail.com>
2024-07-18 11:12:30 +0530

    fix: changed /exercise to /exercises.

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const __dirname = path.dirname(__filename);
 
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 
-app.use("/exercise", exerciseRoutes);
+app.use("/exercises", exerciseRoutes);
 app.use("/bodyParts", bodyPartsRoutes);
 app.use("/targetMuscles", targetMusclesRoutes);
 app.use("/equipments", equipmentsRoutes);
@@ -28,7 +28,8 @@ app.use("/routines", routinesRoutes);
 
 app.use("/", (req, res) => {
   res.send({
-    message: "welcome to body works api",
+    message:
+      "Welcome to body works api! Review all the endpoints here : https://github.com/Akshat-Jaiswal-8/body-works-api.git",
   });
 });
 
