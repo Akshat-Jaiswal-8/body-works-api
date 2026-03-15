aaddab5e4efd590cad6cf653983a27babf373359
Akshat Jaiswal <akshatjaiswal.official@gmail.com>
2026-03-15 12:36:20 +0530

    refactor(eslint): update ESLint configuration to use defineConfig

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,15 @@
 import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default defineConfig(
   {
     ignores: ['dist/**', 'node_modules/**'],
   },
   js.configs.recommended,
-  ...tseslint.configs.recommended,
+  tseslint.configs.recommended,
   {
     files: ['**/*.ts'],
     languageOptions: {
